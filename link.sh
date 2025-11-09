3b6e0cb583b5007bbc989381ee7a61667712be03
Tim Habermaas <tim.habermaas@gmail.com>
2025-11-09 22:24:02 +0100

    Switch to hx as terminal editor

--- link.sh
+++ link.sh
@@ -11,6 +11,7 @@ mkdir -p $HOME/.config/helix
 ln -sf $PWD/ghostty/config $HOME/.config/ghostty/config
 ln -sf $PWD/fish/aliases.fish $HOME/.config/fish/conf.d/aliases.fish 
 ln -sf $PWD/fish/vi-bindings.fish $HOME/.config/fish/conf.d/vi-bindings.fish 
+ln -sf $PWD/fish/environment.fish $HOME/.config/fish/conf.d/environment.fish
 # file called config.toml.tmpl because otherwise mise wants to use it as a
 # project-local configuration file
 ln -sf $PWD/mise/config.toml.tmpl $HOME/.config/mise/config.toml
