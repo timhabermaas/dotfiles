a0830b588c70d823d404ce0b0dd55e93b5c9e81c
Tim Habermaas <tim.habermaas@gmail.com>
2025-11-09 13:16:48 +0100

    Ensure directories exist

--- link.sh
+++ link.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+mkdir -p $HOME/.config/ghostty
+mkdir -p $HOME/.config/fish/conf.d
+mkdir -p $HOME/.config/mise
+mkdir -p $HOME/.config/git
+
+ln -sf $PWD/ghostty/config $HOME/.config/ghostty/config
 ln -sf $PWD/fish/aliases.fish $HOME/.config/fish/conf.d/aliases.fish 
 ln -sf $PWD/fish/vi-bindings.fish $HOME/.config/fish/conf.d/vi-bindings.fish 
 ln -sf $PWD/mise/config.toml.tmpl $HOME/.config/mise/config.toml
-ln -sf $PWD/ghostty/config $HOME/.config/ghostty/config
 ln -sf $PWD/git/gitconfig $HOME/.config/git/config
