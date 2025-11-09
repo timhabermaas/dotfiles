fdd8b0315fb646b9bc51a96d26e58665370beb77
Tim Habermaas <tim.habermaas@gmail.com>
2025-11-09 21:51:19 +0100

    helix config

--- link.sh
+++ link.sh
@@ -6,9 +6,13 @@ mkdir -p $HOME/.config/ghostty
 mkdir -p $HOME/.config/fish/conf.d
 mkdir -p $HOME/.config/mise
 mkdir -p $HOME/.config/git
+mkdir -p $HOME/.config/helix
 
 ln -sf $PWD/ghostty/config $HOME/.config/ghostty/config
 ln -sf $PWD/fish/aliases.fish $HOME/.config/fish/conf.d/aliases.fish 
 ln -sf $PWD/fish/vi-bindings.fish $HOME/.config/fish/conf.d/vi-bindings.fish 
+# file called config.toml.tmpl because otherwise mise wants to use it as a
+# project-local configuration file
 ln -sf $PWD/mise/config.toml.tmpl $HOME/.config/mise/config.toml
 ln -sf $PWD/git/gitconfig $HOME/.config/git/config
+ln -sf $PWD/helix/config.toml $HOME/.config/helix/config.toml
