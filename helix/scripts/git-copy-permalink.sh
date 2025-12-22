c3007a054871cd4ab43b3e9144ef8a03a9afe1fc
Tim Habermaas <tim.habermaas@gmail.com>
2025-12-22 16:19:46 +0100

    Fix permalink script on Linux

--- helix/scripts/git-copy-permalink.sh
+++ helix/scripts/git-copy-permalink.sh
@@ -7,14 +7,13 @@ GIT_REPO_LINK="https://$(echo $GIT_REMOTE | sed 's/^git@//' | sed 's/:/\//' | se
 GIT_COMMIT_SHA=$(git rev-parse HEAD)
 GITHUB_LINK="$GIT_REPO_LINK/blob/$GIT_COMMIT_SHA/$1#L$2-L$3"
 
-echo $GITHUB_LINK | pbcopy
-
 if command -v pbcopy >/dev/null 2>&1; then
     # macOS
     printf "%s" "$GITHUB_LINK" | pbcopy
 elif command -v xclip >/dev/null 2>&1; then
     # Linux with xclip
-    printf "%s" "$GITHUB_LINK" | xclip -selection clipboard
+    # Redirect stdout to /dev/null to prevent shell hanging, see https://emacs.stackexchange.com/questions/39019/xclip-hangs-shell-command#comment61607_39023
+    printf "%s" "$GITHUB_LINK" | xclip -selection clipboard &> /dev/null
 elif command -v xsel >/dev/null 2>&1; then
     # Linux with xsel
     printf "%s" "$GITHUB_LINK" | xsel --clipboard --input
