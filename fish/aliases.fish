1717f6b7546cd182623fc5bdd1b2292df1456694
Tim Habermaas <tim.habermaas@gmail.com>
2026-07-09 09:26:50 +0200

    gpl and gph aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,15 +3,15 @@ alias c="git commit --verbose"
 alias p="git add -p"
 alias d="git diff"
 alias pu="git push"
-alias co="git checkout"
 
 alias tf="terraform"
 
 alias n="nvim ."
 
-alias lzd="lazydocker"
-
 # Rerun a command you've recently executed
 alias rr="eval (history | fzf)"
 # The grep ignores the current branch (marked with *)
 alias gc="git branch --sort=-committerdate | grep -v '^\*' | fzf | xargs git checkout"
+
+alias gpl="git pull"
+alias gph="git push"
